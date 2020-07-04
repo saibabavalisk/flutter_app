cfccb6a11038ebfd8d323c666c5e0c4655550720
saibabavalisk <38373665+saibabavalisk@users.noreply.github.com>
2020-07-04 21:30:33 +0530

    Text widget tutorial

--- lib/app_screens/home_page.dart
+++ lib/app_screens/home_page.dart
@@ -8,62 +8,7 @@ class Home extends StatelessWidget {
       alignment: Alignment.center,
       color: Colors.purple,
       padding: EdgeInsets.all(30.0),
-      child: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(10),
-            child: Row(
-              children: <Widget>[
-                Text("Name",
-                    style: TextStyle(
-                        color: Colors.white,
-                        decoration: TextDecoration.none,
-                        fontFamily: "Raleway",
-                        fontSize: 35.0,
-                        fontWeight: FontWeight.w100)),
-                Text("Location",
-                    style: TextStyle(
-                        color: Colors.white,
-                        decoration: TextDecoration.none,
-                        fontFamily: "Raleway",
-                        fontSize: 35.0,
-                        fontWeight: FontWeight.w100))
-              ],
-            ),
-          ),
-          Row(
-            children: <Widget>[
-              Text("Baba",
-                  style: TextStyle(
-                      color: Colors.white,
-                      decoration: TextDecoration.none,
-                      fontFamily: "Raleway",
-                      fontSize: 35.0,
-                      fontWeight: FontWeight.w100)),
-              Text("Banglore",
-                  style: TextStyle(
-                      color: Colors.white,
-                      decoration: TextDecoration.none,
-                      fontFamily: "Raleway",
-                      fontSize: 35.0,
-                      fontWeight: FontWeight.w100))
-            ],
-          )
-        ],
-      ),
-    )
-
-        //  Container(
-        //   color: Colors.deepPurple,
-        //   alignment: Alignment.center,
-        //   child: Text("Smaple App Body",
-        //       style: TextStyle(
-        //           color: Colors.white,
-        //           decoration: TextDecoration.none,
-        //           fontFamily: "Raleway",
-        //           fontSize: 12.0,
-        //           fontWeight: FontWeight.w100)),
-        // )
-        ;
+      child: Text("Simple App"),
+    );
   }
 }
