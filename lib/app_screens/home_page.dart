f8ff1e3e618f0be3f0246a18b41592580e66bf2b
saibabavalisk <38373665+saibabavalisk@users.noreply.github.com>
2020-07-20 20:45:46 +0530

    text widget with functions

--- lib/app_screens/home_page.dart
+++ lib/app_screens/home_page.dart
@@ -1,14 +1,43 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      color: Colors.purple,
-      padding: EdgeInsets.all(30.0),
-      child: Text("Simple App"),
-    );
-  }
-}
+// class Home extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       color: Colors.purple,
+//       padding: EdgeInsets.all(30.0),
+//       // child: Text(
+//       //   "Simple App",
+//       //   textAlign: TextAlign.center
+//       // )
+//       child: MyAssertImage(),
+//     );
+//   }
+// }
+
+// class MyAssertImage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//         height: 300.0,
+//         color: Colors.black12,
+//         child: RaisedButton(
+//           onPressed: () => showAlertDailog(context),
+//           elevation: 6.0,
+//           child: Text("Raised Button"),
+//         ));
+//   }
+
+//   void showAlertDailog(BuildContext context) {
+//     var alert = AlertDialog(
+//       title: Text("Hi"),
+//       content: Text("Sai Baba"),
+//     );
+
+//     showDialog(
+//       context: context,
+//       builder: (context) => alert,
+//     );
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
-import 'app_screens/home_page.dart';
 
-void main() => runApp(MaterialApp(
-      title: "Sample App",
-      home: Home(),
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // StatelessWidget turns this class into a widget
+  Widget build(BuildContext context) {
+    return Center(
+        child: Text(
+      "Value ${optional_D(1)}",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(fontSize: 20.0),
     ));
+  }
+}
+
+int optional_R(int a, int b, int c) {
+  return a * b * c;
+}
+
+// int optional_P([int a, int b, int c]) {
+//   print(c);
+//   return 1;
+// }
+
+// int optional_N(int a, {int b, int c}) {
+//   return (a * b) + c;
+// }
+
+int optional_D(int a, {int b = 2, int c = 3}) {
+  return a * b * c;
+}
